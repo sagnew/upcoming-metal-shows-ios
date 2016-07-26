dc943662fcb9ab34e0636a07c8e8a5fa884a3b24
Sam Agnew <sagnew@eden.rutgers.edu>
2016-07-26 14:39:26 -0400

    Initial commit

---
+++ UpcomingMetalShows/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  UpcomingMetalShows
+//
+//  Created by Sam Agnew on 7/25/16.
+//  Copyright © 2016 Sam Agnew. All rights reserved.
+//
+
+import UIKit
+import Kanna
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
