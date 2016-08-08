db692be083c3b743cc5ba42346d08a8d5537db27
Sam Agnew <sagnew@eden.rutgers.edu>
2016-08-08 15:05:04 -0400

    Add struct to represent shows

---
+++ Show.swift
@@ -0,0 +1,9 @@
+//
+//  Show.swift
+//  UpcomingMetalShows
+//
+//  Created by Sam Agnew on 8/8/16.
+//  Copyright © 2016 Sam Agnew. All rights reserved.
+//
+
+import Foundation
---
+++ UpcomingMetalShows/Show.swift
@@ -0,0 +1,14 @@
+//
+//  Show.swift
+//  UpcomingMetalShows
+//
+//  Created by Sam Agnew on 8/8/16.
+//  Copyright © 2016 Sam Agnew. All rights reserved.
+//
+
+import Foundation
+
+struct Show {
+    var date: String = ""
+    var description: String = ""
+}
